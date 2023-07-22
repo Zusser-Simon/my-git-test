9fb6405079b45a936f3aef1c54f67d6deaf1ec41
Zusser-Simon <zzyehtet@gmail.com>
2023-07-22 13:41:43 +0630

    first

---
+++ python/zack.py
@@ -0,0 +1,21 @@
+print('Hello World')
+
+name = 'Zack'
+print(name)
+
+x,y,z = 10,20,30
+print(x)
+print(y)
+print(z)
+
+a,b = 1,2
+print(a+b)
+
+a,b = 4,3
+print(a-b)
+
+a,b = 5,6
+print(a*b)
+
+a,b = 3,2
+print(a%b)
---
+++ python/zack2.py
@@ -0,0 +1,17 @@
+name1 = 'zack'
+print(type(name1))
+
+name2 = 2
+print(type(name2))
+
+name3 = 2.5
+print(type(name3))
+
+name4 = True
+print(type(name4))
+
+name1,name2,name3,name4 = 'zack',2,2.5,True
+print(type(name1))
+print(type(name2))
+print(type(name3))
+print(type(name4))
